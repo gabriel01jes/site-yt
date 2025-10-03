54586ecbf4d78a8755153532d2e4a15d95d30626
gabriel01jes <gabriel.jesuz.santos@escola.pr.gov.br>
2025-10-03 14:57:20 -0300

    update

--- alan.js
+++ alan.js
@@ -5,14 +5,14 @@ let trocou = false;
 
 // Lista com os novos caminhos das imagens
 const novasImagens = {
-    "alan1.jpeg": "1.jpeg",
-    "alan2.jpeg": "2.jpeg",
-    "alan3.jpeg": "3.jpeg",
-    "alan4.jpeg": "4.jpeg",
-    "alan5.jpeg": "5.jpeg",
-    "alan6.jpeg": "6.jpeg",
-    "alanlogo.jpeg": "1logo.jpeg",
-    "alancima.jpeg": "2cima.jpeg"
+    "alan1.png": "d1.jpeg",
+    "alan2.png": "d2.jpeg",
+    "alan3.png": "d3.jpeg",
+    "alan4.png": "d4.jpeg",
+    "alan5.png": "d5.jpeg",
+    "alan6.png": "d6.jpeg",
+    "alanlogo.png": "d1logo.jpeg",
+    "alancima.png": "d2cima.jpeg"
 };
 
 botaoImagens.addEventListener("click", () => {
